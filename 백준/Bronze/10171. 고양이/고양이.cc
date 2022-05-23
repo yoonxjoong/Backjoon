f75a4a2ff4163942a4b00cd225fa77f4480ba8c9
Eric <46011674+YHJOONG@users.noreply.github.com>
2022-05-23 22:52:37 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>	// 또는 <cstdio>
+ 
+int main(int argc, char const *argv[]) {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}

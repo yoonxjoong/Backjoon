c51e60a67b10bfc22f9f63f46f502cb34cd26a42
Eric <46011674+YHJOONG@users.noreply.github.com>
2022-06-28 23:32:51 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// DFS를 활용한 백트래킹 문제 
+// 백트래킹 : 해를 찾아가는 탐색을 하는 도중 지금 탐색 중인 경로가 해가 되지 않을 것 같으면 더 탐색하지 않고 되돌아 가는 기법
+// 불필요한 탐색 연산 횟수를 줄임
+// 완전 탐색 기법에 기반하며 완전 탐색 기법 중 하나로 DFS를 사용
+// DFS는 모든 가능한 해(경로)를 탐색 하는 반면, 백트래킹은 불필요한 경로는 사전에 차단하여 탐색하지 않고 최대한 올바른 해를 탐색합니다. 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MAX 9
+using namespace std;
+int N, M;
+vector<int> graph;
+int main() {
+	cin >> N >> M;
+	
+	for(int i=1; i<= N; i++){
+		graph.push_back(i);
+	}
+
+	vector<bool> tmp(graph.size(), false);
+
+	for(int i=0; i < M; i++){
+		tmp[i] = true;
+	}
+
+
+	do{
+		for(int i =0; i < graph.size(); i++){
+			if(tmp[i] == true){
+				cout << graph[i] << " ";
+			}
+		}
+		cout << "\n";
+	}while(prev_permutation(tmp.begin(), tmp.end()));
+
+	return 0;
+}

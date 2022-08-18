bcf0cfafd37dceac62b1da5cb36990cc07a91437
Eric <46011674+YHJOONG@users.noreply.github.com>
2022-08-18 16:27:23 +0900

    [Bronze III] Title: 최댓값, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,6 +1,5 @@
-num_list = []
-for i in range(9):
-    num_list.append(int(input()))
-    
-print(max(num_list))
-print(num_list.index(max(num_list))+1)
+import sys
+data = [int(sys.stdin.readline()) for i in range(9)]
+ret = max(data)
+print(ret)
+print(data.index(ret)+1)

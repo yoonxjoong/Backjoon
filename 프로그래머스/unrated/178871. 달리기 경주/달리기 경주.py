182f98c4675507a4e4956c7316921313a015fd54
YHJOONG <46011674+YHJOONG@users.noreply.github.com>
2023-06-17 18:09:45 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,19 @@
+def solution(players, callings):
+    index_dict = {idx: player for idx, player in enumerate(players)}
+    value_dict = {player: idx for idx, player in enumerate(players)}
+
+    for calling in callings:
+        index = value_dict[calling]
+        if index == 0:
+            continue
+        tmp_index = index_dict[index - 1]
+        tmp_value = index_dict[index - 1]
+        index_dict[index - 1] = calling
+        index_dict[index] = tmp_index
+        
+        value_dict[calling] -= 1
+        value_dict[tmp_value] += 1
+        
+      
+    
+    return list(index_dict.values())
